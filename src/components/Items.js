b4318cf0ac2768aa59f14b14e123fae0d39b0d7b
Cernan Bernardo <cernanb@gmail.com>
2018-02-12 21:33:34 -0800

    Connect packed items container and unpacked items container to redux

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import Item from './Item';
-import Filter from './Filter';
+import React, { Component } from 'react'
+import Item from './Item'
+import Filter from './Filter'
 
 class Items extends Component {
   state = {
     searchTerm: '',
-  };
+  }
 
   updateSearchTerm = searchTerm => {
-    this.setState({ searchTerm });
-  };
+    this.setState({ searchTerm })
+  }
 
   render() {
-    const { title, items, onCheckOff, onRemove } = this.props;
-    const { searchTerm } = this.state;
+    const { title, items, onCheckOff, onRemove } = this.props
+    const { searchTerm } = this.state
     return (
       <section className="Items">
         <h2>
@@ -22,19 +22,19 @@ class Items extends Component {
         <Filter searchTerm={searchTerm} onChange={this.updateSearchTerm} />
         {items
           .filter(item =>
-            item.value.toLowerCase().includes(searchTerm.toLowerCase()),
+            item.value.toLowerCase().includes(searchTerm.toLowerCase())
           )
           .map(item => (
             <Item
               key={item.id}
-              onCheckOff={() => onCheckOff(item)}
-              onRemove={() => onRemove(item)}
+              onCheckOff={() => onCheckOff(item.id)}
+              onRemove={() => onRemove(item.id)}
               item={item}
             />
           ))}
       </section>
-    );
+    )
   }
 }
 
-export default Items;
+export default Items
